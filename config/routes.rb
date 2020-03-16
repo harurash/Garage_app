c5ae50f6e9190e37e14491940b0da99a9d843ba6
harurash <dgkteriyama@gmail.com>
2020-03-16 16:28:47 +0900

    responsive調整
    reposiveに対応できる様に実装。
    投稿画面のcss

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     get "sign_out", :to => "users/sessions#destroy" 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tweets do
-    resource :favorites,only: [ :create, :destroy]
+  resources :tweets, only: [:index,:new,:create,:destroy] do
+    resource :favorites,only:[ :create, :destroy]
     resources :images,only: [:create]
   end
   resources :users
