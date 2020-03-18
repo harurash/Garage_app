5c39acfc4875fa7495bf229bfc926b849313cfe8
harurash <dgkteriyama@gmail.com>
2020-03-18 17:29:05 +0900

    pickupの実装
    いいねのランキングを実装

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -6,4 +6,7 @@ module TweetsHelper
   def sub_image(tweet)
     @sub_image = Image.where(tweet_id: tweet.id)
   end
+  def search_image(tweet)
+    @rank_image = Image.find_by(tweet_id: tweet.id)
+  end
 end
